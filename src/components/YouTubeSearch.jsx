75073b879e4fce5d26a8fc274a57f4f323d9d946
P.Jaswanth Krishna <jaswanthuchiha69@gmail.com>
2025-07-25 22:57:30 +0530

    ui is modified even more better

--- src/components/YouTubeSearch.jsx
+++ src/components/YouTubeSearch.jsx
@@ -34,11 +34,14 @@ const YouTubeSearch = ({ onVideoSelect }) => {
         <input
           type="text"
           className="search-input"
-          placeholder="Enter YouTube URL or video ID (11 characters)"
+          placeholder="Enter YouTube URL or video ID"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" style={{ marginRight: '5px' }}>
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
           Load Video
         </button>
       </form>
